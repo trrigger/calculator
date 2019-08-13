c22f0b477d75eda235c71be566863e5feef2640e
Alena <tiggercake@gmail.com>
2019-08-13 15:31:51 +0300

    Add files via upload

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    public void start(){
+        System.out.println("Calculator started");
+    }
+
+    public void showMenu() {
+        
+    }
+
+    public void readOperant() {}
+
+    public void addMethod() {}
+
+    public void deductMethod() {}
+
+    public void divideMethod() {}
+
+    public void multiplyMethod() {}
+
+    public void quitMethod() {}
+}
---
+++ calculator/src/CalculatorApplication.java
@@ -0,0 +1,8 @@
+public class CalculatorApplication {
+    public static void main (String [] ars )  {
+        Calculator calculator = new Calculator();
+
+        calculator.start();
+
+    }
+}
